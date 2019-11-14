0d48c6a0f302a024629cc9eddc6b8e04a89532ed
Kensuke Shibata <kensukeshibata@gmail.com>
2019-11-14 16:53:42 +0900

    created custom button component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 
 import "./sign-in.styles.scss";
 
@@ -36,7 +37,6 @@ class SignIn extends Component {
             label="email"
             required
           />
-          <label>Email</label>
           <FormInput
             name="password"
             type="password"
@@ -45,8 +45,7 @@ class SignIn extends Component {
             label="password"
             required
           />
-          <label>Email</label>
-          <input type="submit" value="Submit Form"></input>
+          <CustomButton type="submit">Sign in</CustomButton>
         </form>
       </div>
     );
