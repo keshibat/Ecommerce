fa3e7b555a306e9255dc021bf4bd0e42d0ca4084
Kensuke Shibata <kensukeshibata@gmail.com>
2019-11-25 19:45:31 +0900

    updating our application with reselect, replaced cart selectors with memoized selector

--- src/sample.js
+++ src/sample.js
@@ -1,58 +1,14 @@
-import React from "react";
+const CartDropdown = ({ cartItems }) => (
+  <div className="cart-dropdown">
+    <div className="cart-items">
+      {cartItems.map(cartItem => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
+    </div>
+    <CustomButton>GO TO CHECKOUT</CustomButton>
+  </div>
+);
 
-import FormInput from "../form-input/form-input.component";
-
-import "./sign-in.styles.scss";
-
-class SignIn extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      email: "",
-      password: ""
-    };
-  }
-
-  handleSubmit = event => {
-    event.preventDefault();
-
-    this.setState({ email: "", password: "" });
-  };
-
-  handleChange = event => {
-    const { value, name } = event.target;
-
-    this.setState({ [name]: value });
-  };
-
-  render() {
-    return (
-      <div className="sign-in">
-        <h2>I already have an account</h2>
-        <span>Sign in with your email and password</span>
-
-        <form onSubmit={this.handleSubmit}>
-          <FormInput
-            name="email"
-            type="email"
-            handleChange={this.handleChange}
-            value={this.state.email}
-            label="email"
-            required
-          />
-          <FormInput
-            name="password"
-            type="password"
-            value={this.state.password}
-            handleChange={this.handleChange}
-            label="password"
-            required
-          />
-        </form>
-      </div>
-    );
-  }
-}
-
-export default SignIn;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems
+});
