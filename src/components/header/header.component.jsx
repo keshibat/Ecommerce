8be17bb8e327e2f24b44dde50ef540504e66a51a
Kensuke Shibata <kensukeshibata@gmail.com>
2019-11-26 15:45:34 +0900

    fixed merge conflict2

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,14 +6,8 @@ import { createStructuredSelector } from "reselect";
 import { auth } from "../../firebase/firebase.utils";
 import CartIcon from "../cart-icon/cart-icon.component";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
-<<<<<<< HEAD
 import { selectCartHidden } from "../../redux/cart/cart.selectors";
 import { selectCurrentUser } from "../../redux/user/user.selectors";
-||||||| merged common ancestors
-=======
-import { selectCartHidden } from "../../redux/cart/cart.selectors";
-import { selectCurrentUser } from "../../redux/user/user.selectors.js";
->>>>>>> c321f0c214870804677a13cb387397533e8f6f9b
 
 import "./header.styles.scss";
 
